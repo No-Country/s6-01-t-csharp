58ab5df3dc488e16391d84efa28939e35350be27
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-02-14 19:57:09 -0300

    se crea el componente EnlacesNav para dar efectos al nav

---
+++ ClientApp/src/components/EnlacesNav.jsx
@@ -0,0 +1,46 @@
+
+
+const style = {
+    a: "text-textLight text-xl ",
+}
+
+export const EnlacesNav = ( { index, className, onClick } ) => {
+
+    const agregarTitulo = (id, val1, val2, val3, val4) => {
+        
+        switch (id) {
+            case 0:{
+                let titulo = val1;
+                return titulo;
+                }
+            case 1:{
+                let titulo = val2;
+                return titulo;
+            }
+            case 2:{
+                let titulo = val3;
+                return titulo;
+            }
+            case 3:{
+                let titulo = val4;
+                return titulo;
+            }
+            default:{ 
+                let titulo = " ";
+                return titulo;
+            }
+        }
+    }
+
+
+    return(<>
+        <li className= { className }>
+            <a 
+                className={ style.a } 
+                href={ agregarTitulo(index, "#", "#nosotros", "#consulta", "#contacto") }
+                onClick={ onClick }>
+                    { agregarTitulo(index, "Home", "Nosotros", "Consulta", "Contacto") }
+                </a>
+        </li>
+    </>)
+}
--- ClientApp/src/components/Nav.jsx
+++ ClientApp/src/components/Nav.jsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/images/Logo.png";
+import { EnlacesNav } from "./EnlacesNav";
 
 const style = {
     nav: "h-[10vh] w-full fixed z-50 ",
     wrapper_nav:" h-full mx-auto  px-10 flex items-center justify-between w-full max-w-screen-2xl transition-all duration-300 ease-in-out",
     ul:"w-[60%] flex justify-evenly",
-    li:"  h-full  py-1",
+    li:"  h-full  py-1 hover:border-b-4 border-primary transition-all duration-200 ease-in- ",
     btn_login: "bg-primary  w-40 py-2 px-4 rounded-lg text-textLight text-xl",
-    a: "text-textLight text-xl ",
     bg_transparent: "bg-transparent",
     bg_active: "bg-alterno",
     border_bottom:"border-b-4 border-primary",
 }
 
+const enlaces = [0, 1, 2, 3 ,4 ];
+
+
 export const Nav = () => {
 
     const [bgTransparente, setBgTransparent ] = useState(true);
-    const [borderBottom, setBorderBottom] = useState(true);
+
+    const [idEnlace, setIdEnlace] = useState(0);
+
+    const cambiarEnlace = id => {
+        setIdEnlace(id)
+    }
+    
 
     const addBgColor = () => {
         if(window.scrollY > 90 ){
@@ -37,21 +46,13 @@ export const Nav = () => {
                 </a>
                 
                 <ul className={ style.ul } >
-                    <li className= { `${style.li} ${window.location.pathname === "/" ? style.border_bottom : ""}` }>
-                        <a className={ style.a } href="#">Home</a>
-                    </li>
-                    <li className= { `${style.li} ${window.location.pathname === "/#servicios" ? style.border_bottom : ""}` }>
-                        <a className={ style.a } href="#servicios">Servicios</a>
-                    </li>
-                    <li className= { `${style.li} ${window.location.pathname === "/#nosotros" ? style.border_bottom : ""}` }>
-                        <a className={ style.a } href="#nosotros">Nosotros</a>
-                    </li>
-                    <li className= { style.li }>
-                        <a className={ style.a } href="#consultas">Consultas</a>
-                    </li>
-                    <li className= { style.li }>
-                        <a className={ style.a } href="#contacto">Contacto</a>
-                    </li>
+                    { enlaces.map( el => (
+                        <EnlacesNav
+                            className={`${style.li} ${idEnlace === el ? style.border_bottom : ""}`}
+                            key={el} 
+                            index={el}
+                            onClick={ () => cambiarEnlace(el)}/>
+                    ))}
                 </ul>
                 <Link to="/login"> 
                     <button className= { style.btn_login }>Iniciar sesión </button>

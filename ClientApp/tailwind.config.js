9951e785206f555df90208efbfdd9bceba8b2692
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-02-15 20:30:50 -0300

    tailwind modificacion

--- ClientApp/tailwind.config.js
+++ ClientApp/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#01B1B0",
+        secondary: "#541440",
+        alterno: "#176C6D",
+        textLight: "#FCFBFE",
+        textDark: "#11111",
+      },
+    },
   },
   plugins: [],
 }

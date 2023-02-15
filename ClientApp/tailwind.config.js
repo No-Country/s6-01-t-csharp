f377bc54cab40cc6c6f40667563818f71f148c71
Ana Juárez <lisa13suarez@gmail.com>
2023-02-14 22:35:44 -0600

    agregando personalizacion

--- ClientApp/tailwind.config.js
+++ ClientApp/tailwind.config.js
@@ -5,8 +5,34 @@ module.exports = {
     extend: {
       colors: {
         "principal": "#01B1B0",
+        "dark": "#176C6D"
       }
     },
+    screens: {
+      phone: '390px',
+      computer: '1024px'
+    },
+    fontFamily: {
+      'poppins': ['poppins', 'Georgia', 'sans']
+    },
+    margin: {
+      '100%': '100%',
+      '50%': '50%',
+      '20%': '20%'
+    },
+    width: {
+      '80vw': '60vw',
+      '20%': '20%',
+      '22%': '22%',
+      '50%': '50%',
+      '60%': '60%',
+      '40%': '40%',
+      '5%': '5%',
+      '10%': '10%',
+      '100%': '100%',
+      '6': '1.5rem;',
+      '70vw': '70vw',
+    }
   },
   plugins: [],
 }

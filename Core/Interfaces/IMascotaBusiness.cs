d353e13f70b5f59e702c96a499f3ac20c51cd94e
Edijosmen <naruto-capitulo1@hotmail.com>
2023-02-18 16:02:20 -0500

    enpoint base-sin implentacion

---
+++ Core/Interfaces/IMascotaBusiness.cs
@@ -0,0 +1,16 @@
+﻿using s6_01.Core.Helper;
+using s6_01.Core.Models.Dtos;
+using s6_01.Entities;
+
+namespace s6_01.Core.Interfaces
+{
+    public interface IMascotaBusiness
+    {
+        Task<Response<MascotaDto>> GetByIdAsycn(string Id);
+        Task<Response<IEnumerable<MascotaDto>>> Get_AllAsycn(MascotaDto entity);
+        Task<Response<bool>> CreateAsycn(MascotaDto entity);
+        Task<Response<bool>> UpdateAsycn(MascotaDto entity);
+        Task<Response<bool>> DeleteAsycn(string Id);
+
+    }
+}
---
+++ Core/Interfaces/IPaseoBusiness.cs
@@ -0,0 +1,14 @@
+﻿using s6_01.Core.Helper;
+using s6_01.Core.Models.Dtos;
+
+namespace s6_01.Core.Interfaces
+{
+    public interface IPaseoBusiness
+    {
+        Task<Response<PaseoDto>> GetByIdAsycn(string Id);
+        Task<Response<IEnumerable<PaseoDto>>> Get_AllAsycn(PaseoDto entity);
+        Task<Response<bool>> CreateAsycn(PaseoDto entity);
+        Task<Response<bool>> UpdateAsycn(PaseoDto entity);
+        Task<Response<bool>> DeleteAsycn(string Id);
+    }
+}

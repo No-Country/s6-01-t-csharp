89720c3506a1350d7b607762604de66a9f996621
JOSE MANUEL UGALDE BORBOLLA <jmugaldeb@gmail.com>
2023-02-23 08:40:00 -0600

    UpdateToFixSeed

--- DataAccess/Seeds/Paseador/PaseadorSeed.cs
+++ DataAccess/Seeds/Paseador/PaseadorSeed.cs
@@ -20,12 +20,6 @@ namespace s6_01.DataAccess.Seeds
                     Telefono = 1234567890,
                     CobroPorHora = 50.0m,
                     Descripcion = "Soy un paseador de perros con más de 5 años de experiencia.",
-                    Disponibilidad = new List<Disponibilidad>
-                    {
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Monday, HoraInicio = 8, HoraFinal = 10 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Wednesday, HoraInicio = 14, HoraFinal = 16 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Friday, HoraInicio = 10, HoraFinal = 12 }
-                    },
                     Preferencias = new List<string>
                     {
                         "Golden Retriever",
@@ -43,12 +37,6 @@ namespace s6_01.DataAccess.Seeds
                     Telefono = 234578901,
                     CobroPorHora = 60.0m,
                     Descripcion = "Soy una paseadora de perros con más de 10 años de experiencia.",
-                    Disponibilidad = new List<Disponibilidad>
-                    {
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Tuesday, HoraInicio = 12, HoraFinal = 14 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Thursday, HoraInicio = 16, HoraFinal = 18 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Saturday, HoraInicio = 8, HoraFinal = 10 }
-                    },
                     Preferencias = new List<string>
                     {
                         "Caniche",
@@ -65,16 +53,6 @@ namespace s6_01.DataAccess.Seeds
                     Direccion = "Calle 45 #23-56",
                     Telefono = 311234567,
                     CobroPorHora = 25000.00m,
-                    Disponibilidad = new List<Disponibilidad>
-                    {
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Monday, HoraInicio = 9, HoraFinal = 12 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Tuesday, HoraInicio = 10, HoraFinal = 14 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Wednesday, HoraInicio = 8, HoraFinal = 10 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Thursday, HoraInicio = 11, HoraFinal = 13 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Friday, HoraInicio = 12, HoraFinal = 15 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Saturday, HoraInicio = 15, HoraFinal = 18 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Sunday, HoraInicio = 14, HoraFinal = 17 }
-                    },
                     Preferencias = new List<string> { "Perros grandes", "Caminatas largas", "Juegos de pelota" },
                     Descripcion = "Soy un amante de los perros y me encanta pasearlos por el parque. Me gusta jugar con ellos y asegurarme de que estén bien hidratados durante la caminata."
                 },
@@ -86,16 +64,7 @@ namespace s6_01.DataAccess.Seeds
                     CorreoElectronico = "luis.hernandez@example.com",
                     Direccion = "Calle 123, Colonia Centro",
                     Telefono = 555134567,
-                    CobroPorHora = 150.0M,
-                    Disponibilidad = new List<Disponibilidad>
-                    {
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Monday, HoraInicio = 8, HoraFinal = 12 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Tuesday, HoraInicio = 13, HoraFinal = 18 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Wednesday, HoraInicio = 8, HoraFinal = 12 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Friday, HoraInicio = 14, HoraFinal = 20 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Saturday, HoraInicio = 10, HoraFinal = 16 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Sunday, HoraInicio = 10, HoraFinal = 16 }
-                    },
+                    CobroPorHora = 150.0M,              
                     Preferencias = new List<string>
                     {
                         "Caminar por el parque",
@@ -113,22 +82,40 @@ namespace s6_01.DataAccess.Seeds
                     Direccion = "Calle 30 #14-32",
                     Telefono = 316876543,
                     CobroPorHora = 20000.00m,
-                    Disponibilidad = new List<Disponibilidad>
-                    {
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Monday, HoraInicio = 10, HoraFinal = 13 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Tuesday, HoraInicio = 9, HoraFinal = 12 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Wednesday, HoraInicio = 11, HoraFinal = 14 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Thursday, HoraInicio = 12, HoraFinal = 15 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Friday, HoraInicio = 13, HoraFinal = 16 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Saturday, HoraInicio = 9, HoraFinal = 11 },
-                        new Disponibilidad { DiaDeLaSemana = DayOfWeek.Sunday, HoraInicio = 11, HoraFinal = 13 }
-                    },
                     Preferencias = new List<string> { "Perros pequeños", "Caminatas cortas", "Juegos de búsqueda" },
                     Descripcion = "Soy una paseadora de perros con experiencia de 2 años, amo a los perros y me encanta caminar con ellos."
                 }
             };
 
             modelBuilder.Entity<Paseador>().HasData(data);
+            modelBuilder.Entity<Paseador>().OwnsMany(e => e.Disponibilidad).HasData(
+                        new Disponibilidad { Id=1, DiaDeLaSemana = DayOfWeek.Monday, HoraInicio = 8, HoraFinal = 10, PaseadorId = 1 },
+                        new Disponibilidad { Id=2, DiaDeLaSemana = DayOfWeek.Wednesday, HoraInicio = 14, HoraFinal = 16, PaseadorId = 1 },
+                        new Disponibilidad { Id=3, DiaDeLaSemana = DayOfWeek.Friday, HoraInicio = 10, HoraFinal = 12, PaseadorId = 1 },
+                        new Disponibilidad { Id=4, DiaDeLaSemana = DayOfWeek.Tuesday, HoraInicio = 12, HoraFinal = 14, PaseadorId = 2 },
+                        new Disponibilidad { Id=5, DiaDeLaSemana = DayOfWeek.Thursday, HoraInicio = 16, HoraFinal = 18, PaseadorId = 2 },
+                        new Disponibilidad { Id=6, DiaDeLaSemana = DayOfWeek.Saturday, HoraInicio = 8, HoraFinal = 10, PaseadorId = 2 },
+                        new Disponibilidad { Id=7, DiaDeLaSemana = DayOfWeek.Monday, HoraInicio = 9, HoraFinal = 12, PaseadorId = 3 },
+                        new Disponibilidad { Id=8, DiaDeLaSemana = DayOfWeek.Tuesday, HoraInicio = 10, HoraFinal = 14, PaseadorId = 3 },
+                        new Disponibilidad { Id=9, DiaDeLaSemana = DayOfWeek.Wednesday, HoraInicio = 8, HoraFinal = 10, PaseadorId = 3 },
+                        new Disponibilidad { Id=10, DiaDeLaSemana = DayOfWeek.Thursday, HoraInicio = 11, HoraFinal = 13, PaseadorId = 3 },
+                        new Disponibilidad { Id=11, DiaDeLaSemana = DayOfWeek.Friday, HoraInicio = 12, HoraFinal = 15, PaseadorId = 3 },
+                        new Disponibilidad { Id=12, DiaDeLaSemana = DayOfWeek.Saturday, HoraInicio = 15, HoraFinal = 18, PaseadorId = 3 },
+                        new Disponibilidad { Id=13, DiaDeLaSemana = DayOfWeek.Sunday, HoraInicio = 14, HoraFinal = 17, PaseadorId = 3 },
+                        new Disponibilidad { Id=14, DiaDeLaSemana = DayOfWeek.Monday, HoraInicio = 8, HoraFinal = 12, PaseadorId = 4 },
+                        new Disponibilidad { Id=15, DiaDeLaSemana = DayOfWeek.Tuesday, HoraInicio = 13, HoraFinal = 18, PaseadorId = 4 },
+                        new Disponibilidad { Id=16, DiaDeLaSemana = DayOfWeek.Wednesday, HoraInicio = 8, HoraFinal = 12, PaseadorId = 4 },
+                        new Disponibilidad { Id=17, DiaDeLaSemana = DayOfWeek.Friday, HoraInicio = 14, HoraFinal = 20, PaseadorId = 4 },
+                        new Disponibilidad { Id=18, DiaDeLaSemana = DayOfWeek.Saturday, HoraInicio = 10, HoraFinal = 16, PaseadorId = 4 },
+                        new Disponibilidad { Id=19, DiaDeLaSemana = DayOfWeek.Sunday, HoraInicio = 10, HoraFinal = 16, PaseadorId = 4 },
+                        new Disponibilidad { Id=20, DiaDeLaSemana = DayOfWeek.Monday, HoraInicio = 10, HoraFinal = 13, PaseadorId = 5 },
+                        new Disponibilidad { Id=21, DiaDeLaSemana = DayOfWeek.Tuesday, HoraInicio = 9, HoraFinal = 12, PaseadorId = 5 },
+                        new Disponibilidad { Id=22, DiaDeLaSemana = DayOfWeek.Wednesday, HoraInicio = 11, HoraFinal = 14, PaseadorId = 5 },
+                        new Disponibilidad { Id=23, DiaDeLaSemana = DayOfWeek.Thursday, HoraInicio = 12, HoraFinal = 15, PaseadorId = 5 },
+                        new Disponibilidad { Id=24, DiaDeLaSemana = DayOfWeek.Friday, HoraInicio = 13, HoraFinal = 16, PaseadorId = 5 },
+                        new Disponibilidad { Id=25, DiaDeLaSemana = DayOfWeek.Saturday, HoraInicio = 9, HoraFinal = 11, PaseadorId = 5 },
+                        new Disponibilidad { Id=26, DiaDeLaSemana = DayOfWeek.Sunday, HoraInicio = 11, HoraFinal = 13, PaseadorId = 5 }
+                );
         }
     }
 }
--- Entities/Disponibilidad.cs
+++ Entities/Disponibilidad.cs
@@ -6,5 +6,6 @@
         public DayOfWeek DiaDeLaSemana { get; set; }
         public int HoraInicio { get; set; }
         public int HoraFinal { get; set; }
+        public int PaseadorId { get; set; }
     }
 }

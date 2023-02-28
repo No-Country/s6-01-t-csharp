7613ca2631e8542b6b5ebf87912d6f3bf61b6f73
JOSE MANUEL UGALDE BORBOLLA <jmugaldeb@gmail.com>
2023-02-28 08:13:31 -0600

    Updated Image VetorChecked

--- ClientApp/src/components/Checked.jsx
+++ ClientApp/src/components/Checked.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import check from "../assets/icons/Vector.png";
+import check from "../assets/icons/VectorCheck.png";
 
 
 
